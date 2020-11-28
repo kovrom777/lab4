b4c09de9685ddd3248d105d374e05dd1f95c2fcf
Roman Kovayin <r.kovaykin@avsw.ru>
2020-11-28 14:55:37 +0300

    Scripted auto-commit on change (2020-11-28 14:55:37) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/JSTestExecuterActor.java
+++ src/main/java/ru/bmstu/iu9/lab4/JSTestExecuterActor.java
@@ -2,10 +2,19 @@ package ru.bmstu.iu9.lab4;
 
 import akka.actor.AbstractActor;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.util.ArrayList;
+
 public class JSTestExecuterActor extends AbstractActor {
 
 
     public final String JS_COMPILER = "nashorn";
 
-    public JSFunctionRes jsExecutor(String jsFunc, String FuncName, String[] js)
+    public JSFunctionRes jsExecutor(String jsFunc, String FuncName, ArrayList<Integer> jsParams, String packageId) throws ScriptException {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName(JS_COMPILER);
+        engine.eval(jsFunc);
+        
+    }
 }

feff2e6f776c25497542f49dc7a4072bd4e74ae3
Roman Kovayin <r.kovaykin@avsw.ru>
2020-11-28 14:33:56 +0300

    Scripted auto-commit on change (2020-11-28 14:33:56) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/MainClass.java
+++ src/main/java/ru/bmstu/iu9/lab4/MainClass.java
@@ -2,10 +2,12 @@ package ru.bmstu.iu9.lab4;
 
 import akka.actor.*;
 
+import java.io.IOException;
+
 
 public class MainClass {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         final ActorSystem system = new ActorSystem.create("Test");
 

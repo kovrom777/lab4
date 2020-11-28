3ed29eae56283da6d9f1e2a31e7e8215509d383b
Roman Kovayin <r.kovaykin@avsw.ru>
2020-11-28 14:52:29 +0300

    Scripted auto-commit on change (2020-11-28 14:52:29) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/JSFunctionRes.java
+++ src/main/java/ru/bmstu/iu9/lab4/JSFunctionRes.java
@@ -2,6 +2,31 @@ package ru.bmstu.iu9.lab4;
 
 public class JSFunctionRes {
 
-    private packageID, 
+    private String packageID, functionRes;
 
+    public JSFunctionRes(String packageID, String functionRes) {
+        this.packageID = packageID;
+        this.functionRes = functionRes;
+    }
+
+    public String getPackageID() {
+        return packageID;
+    }
+
+    public String getFunctionRes() {
+        return functionRes;
+    }
+
+    public void setPackageID(String packageID) {
+        this.packageID = packageID;
+    }
+
+    public void setFunctionRes(String functionRes) {
+        this.functionRes = functionRes;
+    }
+
+    @Override
+    public String toString(){
+        return "JSFunctionRec{" + "packageId = " + packageID + "\'" + ", FunctionRes=" + functionRes + "\'" + "}";
+    }
 }

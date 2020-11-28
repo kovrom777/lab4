f5418cf33a4c446240d63430b313aabb453865f8
Roman Kovayin <r.kovaykin@avsw.ru>
2020-11-28 14:33:22 +0300

    Scripted auto-commit on change (2020-11-28 14:33:22) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/MainClass.java
+++ src/main/java/ru/bmstu/iu9/lab4/MainClass.java
@@ -1,12 +1,22 @@
 package ru.bmstu.iu9.lab4;
 
-import akka.actor.ActorSystem;
+import akka.actor.*;
+import akka.dispatch.Dispatchers;
+import akka.dispatch.Mailboxes;
+import akka.event.EventStream;
+import akka.event.LoggingAdapter;
+import scala.Function0;
+import scala.collection.Iterable;
+import scala.concurrent.ExecutionContextExecutor;
+import scala.concurrent.Future;
+
+import java.util.concurrent.CompletionStage;
 
 public class MainClass {
 
     public static void main(String[] args) {
 
-        final ActorSystem system = new Ac
+        final ActorSystem system = new ActorSystem.create()
 
     }
 

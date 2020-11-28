f0f7b7bcef9d7a3217a9c5b24d2ff3a0a9b74c19
Roman Kovayin <r.kovaykin@avsw.ru>
2020-11-28 15:00:55 +0300

    Scripted auto-commit on change (2020-11-28 15:00:55) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/JSTestExecuterActor.java
+++ src/main/java/ru/bmstu/iu9/lab4/JSTestExecuterActor.java
@@ -1,7 +1,9 @@
 package ru.bmstu.iu9.lab4;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
+import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
@@ -12,9 +14,20 @@ public class JSTestExecuterActor extends AbstractActor {
 
     public final String JS_COMPILER = "nashorn";
 
-    public JSFunctionRes jsExecutor(String jsFunc, String FuncName, ArrayList<Integer> jsParams, String packageId) throws ScriptException {
+    public JSFunctionRes jsExecutor(String jsFunc, String funcName, ArrayList<Integer> jsParams, String packageId) throws ScriptException,
+            NoSuchMethodException {
         ScriptEngine engine = new ScriptEngineManager().getEngineByName(JS_COMPILER);
         engine.eval(jsFunc);
-        
+        Invocable invocable = (Invocable) engine;
+
+        return new JSFunctionRes(packageId,
+                invocable.invokeFunction(funcName, jsParams.toArray()).toString());
+    }
+
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match()
     }
 }

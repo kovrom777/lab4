5bd415b187ede39ae10477e43114426f694f1120
Roman Kovayin <r.kovaykin@avsw.ru>
2020-12-20 21:31:29 +0300

    Scripted auto-commit on change (2020-12-20 21:31:29) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/ActorRouter.java
+++ src/main/java/ru/bmstu/iu9/lab4/ActorRouter.java
@@ -1,12 +1,17 @@
 package ru.bmstu.iu9.lab4;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
 import akka.actor.Props;
 
 public class ActorRouter extends AbstractActor {
 
-    public static int timeout = 3000;
+    public static int TIMEOUT_CONSTANT = 10000;
+
 
     Props propsJSTestExecProp = Props.create(JSTestExecuterActor.class);
+    Props propsResultProp = Props.create(ResultSupplyActor.class);
 
+    ActorRef storeActor = getContext().actorOf(propsResultProp, "Store");
+    ActorRef testActor = getContext().actorOf(new Roun)
 }

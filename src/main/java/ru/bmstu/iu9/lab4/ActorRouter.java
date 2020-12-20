ef551f6f4c336e183f33a00f4299fbe548f76747
Roman Kovayin <r.kovaykin@avsw.ru>
2020-12-20 21:11:46 +0300

    Scripted auto-commit on change (2020-12-20 21:11:46) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/ActorRouter.java
+++ src/main/java/ru/bmstu/iu9/lab4/ActorRouter.java
@@ -2,7 +2,7 @@ package ru.bmstu.iu9.lab4;
 
 import akka.actor.Props;
 
-public class ActorRouter {
+public class ActorRouter extends  {
 
     public static int timeout = 3000;
     Props propsJSExec = Props.create()

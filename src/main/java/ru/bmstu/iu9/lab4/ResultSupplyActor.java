e71ff0ad85e427e8cdcb7dcfc58cc1344dddf804
Roman Kovayin <r.kovaykin@avsw.ru>
2020-12-20 21:21:18 +0300

    Scripted auto-commit on change (2020-12-20 21:21:18) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/ResultSupplyActor.java
+++ src/main/java/ru/bmstu/iu9/lab4/ResultSupplyActor.java
@@ -1,4 +1,49 @@
 package ru.bmstu.iu9.lab4;
 
-public class ResultSupplyActor {
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResultSupplyActor extends AbstractActor {
+
+    private Map<String, ArrayList<String>> store = new HashMap<>();
+
+    private void saveResult(JSFunctionRes res){
+        ArrayList<String> dop;
+        if (store.containsKey(res.getPackageID())){
+            dop = store.get(res.getPackageID());
+        }else{
+            dop = new ArrayList<>();
+        }
+        dop.add(res.getFunctionRes());
+        store.put(res.getPackageID(), dop);
+    }
+
+    private JSFunctionRes printPackageId(String packageId){
+        if (store.containsKey(packageId)){
+            ArrayList<String> dop = store.get(packageId);
+            for (int i = 0; i< dop.size(); i++){
+                return new JSFunctionRes(packageId, dop.get(i));
+            }
+        }
+        return new JSFunctionRes(packageId, "Oups, There are no tests yet");
+    }
+
+    public Receive createReceive(){
+        return ReceiveBuilder()
+                .match(
+                        JSFunctionRes.class,
+                        this::saveResult
+                )
+                .match(
+                        String.class,
+                        id -> {
+
+                        }
+                )
+    }
+
 }

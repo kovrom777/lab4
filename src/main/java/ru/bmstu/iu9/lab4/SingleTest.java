ca29b244e6a7379dbf411bfcb196a1b771003d76
Roman Kovayin <r.kovaykin@avsw.ru>
2020-12-22 17:34:40 +0300

    Scripted auto-commit on change (2020-12-22 17:34:40) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/SingleTest.java
+++ src/main/java/ru/bmstu/iu9/lab4/SingleTest.java
@@ -1,4 +1,54 @@
 package ru.bmstu.iu9.lab4;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+
 public class SingleTest {
+
+
+    private final String TEST_NAME = "testName";
+    private final String EXPECTED_RESULT = "expectedResult";
+    private final String Params = "params";
+
+
+    @JsonProperty(TEST_NAME)
+    public String testName;
+    @JsonProperty(EXPECTED_RESULT)
+    public String expectedResult;
+    @JsonProperty(Params)
+    public ArrayList<Integer> params;
+
+    @JsonCreator
+    public SingleTest( @JsonProperty(TEST_NAME) String testName, @JsonProperty(EXPECTED_RESULT) String expectedResult,
+                       @JsonProperty(Params) ArrayList<Integer> params) {
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.params = params;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public void setTestName(String testName) {
+        this.testName = testName;
+    }
+
+    public String getExpectedResult() {
+        return expectedResult;
+    }
+
+    public void setExpectedResult(String expectedResult) {
+        this.expectedResult = expectedResult;
+    }
+
+    public ArrayList<Integer> getParams() {
+        return params;
+    }
+
+    public void setParams(ArrayList<Integer> params) {
+        this.params = params;
+    }
 }

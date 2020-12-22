e9d9ce5373365d5c817db3981f97022b5cd0dcf8
Roman Kovayin <r.kovaykin@avsw.ru>
2020-12-22 17:57:04 +0300

    Scripted auto-commit on change (2020-12-22 17:57:04) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/MainClass.java
+++ src/main/java/ru/bmstu/iu9/lab4/MainClass.java
@@ -2,6 +2,9 @@ package ru.bmstu.iu9.lab4;
 
 import akka.actor.*;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.server.Route;
+import akka.stream.ActorMaterializer;
+import akka.stream.Materializer;
 
 import java.io.IOException;
 
@@ -13,8 +16,15 @@ public class MainClass {
         final ActorSystem system = ActorSystem.create("Test");
         ActorRef router = system.actorOf(Props.create(ActorRouter.class));
         final Http http = Http.get(system);
-        final AkkaMainApplication application = new AkkaMainApplication();
+        final MainClass application = new MainClass();
+        final Materializer materializer = new ActorMaterializer.create(system);
+//        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = application.
 
+    }
+
+
+    private Route createRoute(ActorSystem actorSystem, ActorRef actorRef){
+        return 
     }
 
 }

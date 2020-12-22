70926a57a80b8f36146205f8715b4a7f48e894d4
Roman Kovayin <r.kovaykin@avsw.ru>
2020-12-22 17:17:10 +0300

    Scripted auto-commit on change (2020-12-22 17:17:10) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab4/JSFuncStore.java
+++ src/main/java/ru/bmstu/iu9/lab4/JSFuncStore.java
@@ -2,6 +2,10 @@ package ru.bmstu.iu9.lab4;
 
 public class JSFuncStore {
 
-    
+    final String PACKAGEID = "packageId";
+    final String FUNCTION = "jsScript";
+    final String FUNCTION_Name = "functionName"
+
+
 
 }
